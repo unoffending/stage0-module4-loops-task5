bf8c51c780d5b19bb4a07c504aa37f4be897af17
unoffending <109032978+unoffending@users.noreply.github.com>
2023-10-20 13:33:12 +0600

    Update Rectangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
-
+        String eight = "8";
+    String empty = " ";
+    for(int i = 0; i < length; i++) {
+        System.out.print(eight);
+    } System.out.println();
+    for(int j = height - 2; j > 0; j--) {
+        System.out.print(eight);
+    for(int i = length - 2; i > 0; i--) {
+        System.out.print(empty);
+    } System.out.print(eight);
+        System.out.println();
+    }
+    for(int i = 0; i < length; i++) {
+        System.out.print(eight);
+    }
+        System.out.println();
     }
 }
