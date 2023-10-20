8d60eb4b2be4f9247fbf782d18c3d85e6a8f907d
unoffending <109032978+unoffending@users.noreply.github.com>
2023-10-20 11:57:03 +0600

    Update Cross.java

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,37 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        String eight = "8";
+        String empty = " ";
+   
+        for(int b = 0; b < sideLength; b++) {
+            for(int c = 0; c < sideLength; c++) {
+                System.out.print(empty);
+            }
+            for(int c = 0; c < sideLength; c++) {
+                System.out.print(eight);
+            }
+            for(int c = 0; c < sideLength; c++) {
+                System.out.print(empty);
+            }
+            System.out.println();
+        } 
+     for(int b = 0; b < sideLength; b++) {
+            for(int c = 0; c < sideLength * 3; c++) {
+                System.out.print(eight);
+            }
+            System.out.println();
+        }  for(int b = 0; b < sideLength; b++) {
+            for(int c = 0; c < sideLength; c++) {
+                System.out.print(empty);
+            }
+            for(int c = 0; c < sideLength; c++) {
+                System.out.print(eight);
+            }
+            for(int c = 0; c < sideLength; c++) {
+                System.out.print(empty);
+            }
+            System.out.println();
+        } 
     }
 }
